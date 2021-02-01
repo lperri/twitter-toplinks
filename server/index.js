831f6b3b3e6f933c997dffe82332613dfb2990ec
Leah Perri <leahperri2017@u.northwestern.edu>
2021-01-31 20:13:39 -0800

    Integrate passport for twitter authentication (#2)

--- server/index.js
+++ server/index.js
@@ -7,6 +7,16 @@ app.get("/", (req, res) => {
   res.send({ hi: "there" });
 });
 
+app.get("/auth/twitter", passport.authenticate("twitter"));
+
+app.get(
+  "/auth/twitter/callback",
+  passport.authenticate("twitter", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+  })
+);
+
 // Heroku sets the PORT environment variable if running in prod
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
