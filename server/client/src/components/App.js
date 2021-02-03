4d8ccba8cf6274a40b664a9d1a0be4646230b8b8
Leah Perri <leahperri2017@u.northwestern.edu>
2021-02-02 22:24:27 -0800

    configure redux to determine if user is signed in

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,10 +1,33 @@
-function App() {
-  return (
-    <div className="App">
-      <p>Twitter TopLinks</p>
-      <a href="/auth/twitter">Sign in with Twitter</a>
-    </div>
-  );
+import React, { Component } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import { connect } from "react-redux";
+import * as actions from "../actions";
+
+import Header from "./Header";
+
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
+const Landing = () => <h2>Landing</h2>;
+
+class App extends Component {
+  componentDidMount() {
+    this.props.fetchUser();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route exact path="/" component={Landing} />
+            <Route path="/login/success" component={SurveyNew} />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
-export default App;
+// passing actions to connect, they get assigned to App component as props
+export default connect(null, actions)(App);
