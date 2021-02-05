c2eb2d6580d601a9e955a7c61b6faf62f5280a82
Leah Perri <leahperri2017@u.northwestern.edu>
2021-02-05 15:46:44 -0800

    greets by name; css improvements

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -11,7 +11,7 @@ module.exports = (app) => {
   app.get(
     "/auth/twitter/callback",
     passport.authenticate("twitter", {
-      successRedirect: "/",
+      successRedirect: "/dashboard",
       failureRedirect: "/",
     })
   );
